6fb9a9ddfdb9562a93bb2c8ec136732f9ac68534
Deepali gupta <95581953+Deepaligup-ta@users.noreply.github.com>
2022-06-07 15:54:21 +0530

    1.js

---
+++ 1.js
@@ -0,0 +1,10 @@
+<html>
+<body>
+<h2>My first java script</h2>
+<button type="button"
+onclick="document.getELEMENTBYID('ID').innerHTML=Date()>
+click me to display date and time</button>
+
+<p id="demo"></p>
+</body>
+</html>
