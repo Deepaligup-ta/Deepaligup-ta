13c4cf029c7c0295f67b0426724bccc706d9f53a
Deepali gupta <95581953+Deepaligup-ta@users.noreply.github.com>
2022-06-07 16:05:47 +0530

    2.js

---
+++ 2.js
@@ -0,0 +1,6 @@
+<html>
+<body>
+<p id="demo">Java script can change html document</p>
+<button type="button" onclick='document.GETELEMENTBYID("demo").innerhtml="hello javascript"'>clickme!</button>
+</body>
+</html>
